1d0b38fb4041650780c6741418affcc400075e4e
Bhawana Kumari <117829849+bhawanak0504@users.noreply.github.com>
2025-02-19 22:48:48 +0530

    Update Login.jsx

--- Client/src/Login.jsx
+++ Client/src/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
   axios.defaults.withCredentials = true;
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:3001/login', { email, password })
+    axios.post('http://maternity-hub.onrender.com/login', { email, password })
       .then(res => {
         if (res.data.status === 'Success') {  // Assuming the response data has a status field
             alert('Login Successful as User');
